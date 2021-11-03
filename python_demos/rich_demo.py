e584507ae1f2982109727800bf81a4029cfcc652
Bruce Campbell <9995804+dr-ochanomizu@users.noreply.github.com>
2021-11-03 19:09:29 -0400

    110321a

--- python_demos/rich_demo.py
+++ python_demos/rich_demo.py
@@ -52,11 +52,39 @@ import time
 
 def do_step(step):
     print(step)
-    time.sleep(random.randint(0, 4))
+    time.sleep(random.randint(0, 2))
 
 
 for step in track(range(10)):
     do_step(step)
 
+import time
 
+from rich.console import Console
+from rich.table import Table
+
+console = Console()
+
+table = Table(show_header=True, header_style="bold magenta")
+table.add_column("Date", style="dim", width=12)
+table.add_column("Title")
+table.add_column("Production Budget", justify="right")
+table.add_column("Box Office", justify="right")
+table.add_row(
+    "Dec 20, 2019", "Star Wars: The Rise of Skywalker", "$275,000,000", "$375,126,118"
+)
+table.add_row(
+    "May 25, 2018",
+    "[red]Solo[/red]: A Star Wars Story",
+    "$275,000,000",
+    "$393,151,347",
+)
+table.add_row(
+    "Dec 15, 2017",
+    "Star Wars Ep. VIII: The Last Jedi",
+    "$262,000,000",
+    "[bold]$1,332,539,889[/bold]",
+)
+
+console.print(table)
 print("done")

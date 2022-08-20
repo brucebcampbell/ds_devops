d5c8b4663b7ef5fddb877dc733c8fc8ca5a53ce6
Bruce Campbell <9995804+campbell-2589@users.noreply.github.com>
2022-08-20 15:08:39 -0400

    Create fall2022_dev.sh

---
+++ ubuntu/fall2022_dev.sh
@@ -0,0 +1,11 @@
+
+sudo apt-get update
+
+sudo apt-get -y install git-all
+sudo apt-get -y install build-essential
+sudo apt-get -y install texlive-full
+sudo apt-get -y install awscli
+#sudo snap install google-cloud-sdk --classic
+
+sudo apt-get -y install gfortran
+
